cf962250feb9f29a1a4f6be5bb9205bbafb6046f
Chris Cadden <cadden.cs@gmail.com>
2024-02-17 08:25:13 -0500

    Exercise 2.3

--- ch2/popcount/main.go
+++ ch2/popcount/main.go
@@ -17,15 +17,25 @@ func init() {
 }
 
 // PopCount returns the population count (number of set bits) of x.
+// func PopCount(x uint64) int {
+// 	return int(pc[byte(x>>(0*8))] +
+// 		pc[byte(x>>(1*8))] +
+// 		pc[byte(x>>(2*8))] +
+// 		pc[byte(x>>(3*8))] +
+// 		pc[byte(x>>(4*8))] +
+// 		pc[byte(x>>(5*8))] +
+// 		pc[byte(x>>(6*8))] +
+// 		pc[byte(x>>(7*8))])
+// }
+
 func PopCount(x uint64) int {
-	return int(pc[byte(x>>(0*8))] +
-		pc[byte(x>>(1*8))] +
-		pc[byte(x>>(2*8))] +
-		pc[byte(x>>(3*8))] +
-		pc[byte(x>>(4*8))] +
-		pc[byte(x>>(5*8))] +
-		pc[byte(x>>(6*8))] +
-		pc[byte(x>>(7*8))])
+  sum := 0
+
+  for i := 0; i < 8; i++ {
+    sum += int(pc[byte(x>>(i*8))])
+  }
+
+  return sum
 }
 
 //!-
